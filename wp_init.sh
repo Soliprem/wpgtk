686f87f9aacd7576cd108f8b6cefdec04018c92e
soliprem <franci.solidoro@gmail.com>
2023-04-22 05:43:33 +0200

    update

--- wp_init.sh
+++ wp_init.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-wpg -rs '1339547-popular-4k-psychedelic-wallpapers-3840x2160-hd-1080p.jpg' '1339547-popular-4k-psychedelic-wallpapers-3840x2160-hd-1080p.jpg'
+wpg -rs 'Binf17Y.jpg' 'Binf17Y.jpg'
