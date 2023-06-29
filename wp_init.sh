1ec119b9aa1ebabb7c97e5a2189695a5236997b7
Soliprem <franci.solidoro@gmail.com>
2023-06-29 12:49:09 +0200

    updated battery critical background

---
+++ wp_init.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+wpg -rs 'MEDIA-star-wars-robot-walker-thing-i-forgot-what-its-called-please-dont-hate-me.jpg' 'MEDIA-star-wars-robot-walker-thing-i-forgot-what-its-called-please-dont-hate-me.jpg'
